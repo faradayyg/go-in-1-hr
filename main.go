6c300fe54972b10dbffaea1d7b4aa2a7fa8bb427
Friday G <godswill.friday@parcellab.com>
2024-06-30 21:03:56 +0200

    Maps

--- main.go
+++ main.go
@@ -6,13 +6,17 @@ import (
 )
 
 func main() {
+	// Typical error handling in GO
 	yeah, err := foo(2)
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
 		fmt.Println(yeah)
 	}
+	testMaps()
+}
 
+func arrays() {
 	// Arrays
 	var arr [3]int32 = [...]int32{0, 1, 2}
 
@@ -45,6 +49,27 @@ func main() {
 	intSlice2 = append(intSlice2, make([]int32, 2)...)
 
 	fmt.Printf("\nNew size of slice %v, capacity %v", len(intSlice2), cap(intSlice2))
+
+}
+
+func testMaps() {
+	// Creating maps (dicts in python)
+	var myMap map[string]int8 = map[string]int8{"Friday": 20}
+	fmt.Println(myMap)
+
+	// Accessing a non-existent item returns the default for the value type
+	fmt.Println(myMap["Godswill"])
+
+	// To mitigate this,  go returns two values when you access a map, (why does print not show  this???)
+	var num, fetched = myMap["Godswill"]
+	fmt.Printf("The value returned is %v, does it exist on the map? %v", num, fetched)
+
+	num, fetched = myMap["Friday"]
+	fmt.Printf("\nThe value returned is %v, does it exist on the map? %v\n", num, fetched)
+
+	// Making maps with slices
+	secondMap := make(map[string]int32)
+	fmt.Println(secondMap)
 }
 
 func foo(num int) (string, error) {
